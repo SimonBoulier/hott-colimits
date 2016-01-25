1f2a9e645828e16634b22d8d47f4719bac6094fd
KevinQuirin <kevin.quirin@gmail.com>
2016-01-25 09:31:11 +0100

    add Colimits.v, loading whole lib

---
+++ Colimits.v
@@ -0,0 +1,17 @@
+Require Import Colimit.
+Require Import Auxiliary.
+Require Import CoEqualizer.
+Require Import Colimit_Prod.
+Require Import Colimit_Sigma.
+Require Import Colimit_trunc.
+Require Import CommutativeSquares.
+Require Import Diagram.
+Require Import MyLemmas.
+Require Import MyTacs.
+
+Require Import IteratedKernelPair.IteratedKernelPair.
+Require Import IteratedKernelPair.KernelPair_v1.
+Require Import IteratedKernelPair.KernelPair_v2.
+Require Import IteratedKernelPair.KPv2_mono.
+Require Import IteratedKernelPair.link_KPv2_CechNerve.
+Require Import IteratedKernelPair.MappingTelescope.
